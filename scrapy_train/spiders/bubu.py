f83e36318d9dbcb6171f61d00d470a7120503a5e
jessie <junchangchen@gmail.com>
2017-07-25 10:56:57 +0800

    update bubu

--- scrapy_train/spiders/bubu.py
+++ scrapy_train/spiders/bubu.py
@@ -38,18 +38,24 @@ class BubuSpider(scrapy.Spider):
         return response.url.endswith("index.xml")
 
     def parse_video(self, response):
-        # print 'parse video'
         videos = response.xpath("//video")
-        result = map(self._parse_video_item, videos)
-        # self.log(result)
-        poster_url = response.url.replace("/".join(response.url.split("/")[-3:]), "1_1.jpg")
-        yield {'videos': result, 'poster': poster_url}
+        url_arr = response.url.split("/")
+        video_dir_url = "/".join(url_arr[:-1])
+        img_dir_url = "/".join(url_arr[:-3])
+
+        def item_parse_map_function((idx, item)):
+            return self._parse_video_item(item, video_dir_url, "{}/1_{}.jpg".format(img_dir_url, str(idx+1)))
+
+        result = map(item_parse_map_function, enumerate(videos))
+        yield {'videos': result, 'poster': "{}/1_1.jpg".format(img_dir_url)}
 
     @staticmethod
-    def _parse_video_item(selector):
+    def _parse_video_item(selector, video_dir_url, poster_url):
+        src = selector.xpath("./file/text()").extract_first().replace('\n', '')
+        filename = src.split("/")[-1:][0]
         return {
-            "src": selector.xpath("./file/text()").extract_first().replace('\n', ''),
+            "src": "{}/{}".format(video_dir_url, filename),
             "size": selector.xpath("./size/text()").extract_first(),
             "duration": selector.xpath("./seconds/text()").extract_first(),
+            "poster": poster_url,
         }
-
